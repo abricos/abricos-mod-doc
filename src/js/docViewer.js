c6a1be473d0bdc98b4c8586b8b0d844a3cebed1e
Alexander Kuzmin <roosit@abricos.org>
2016-10-13 20:56:02 +0300

    release: doc viewer

--- src/js/docViewer.js
+++ src/js/docViewer.js
@@ -1,8 +1,7 @@
 var Component = new Brick.Component();
 Component.requires = {
     mod: [
-        {name: 'sys', files: ['editor.js']},
-        {name: '{C#MODNAME}', files: ['elEditor.js', 'toolbar.js']}
+        {name: '{C#MODNAME}', files: ['lib.js']}
     ]
 };
 Component.entryPoint = function(NS){
@@ -18,25 +17,65 @@ Component.entryPoint = function(NS){
             };
         },
         onInitAppWidget: function(err, appInstance){
-            var tp = this.template,
-            docid = this.get('docid');
+            var docid = this.get('docid');
 
             appInstance.doc(docid, function(err, result){
                 this.set('waiting', false);
                 if (err){
                     return;
                 }
-                var doc = result.doc;
-                tp.setHTML({
-                    title: doc.get('title')
-                });
+                this.set('doc', result.doc);
+                this.renderDoc();
             }, this);
+        },
+        renderDoc: function(){
+            var tp = this.template,
+                doc = this.get('doc');
+
+            var body = this._renderElements(0);
+
+            tp.setHTML({
+                title: doc.get('title'),
+                body: body
+            });
+        },
+        _renderElements: function(parentid){
+            var tp = this.template,
+                lst = "",
+                el, type, id;
+            this.get('doc').elEach(parentid, function(element){
+                id = element.get('id');
+                type = element.get('type');
+                el = element.get('el');
+
+                switch (type) {
+                    case 'page':
+                    case 'section':
+                        lst += tp.replace(type, {
+                            id: id,
+                            title: el.get('title'),
+                            childs: this._renderElements(id)
+                        });
+                        break;
+                    case 'text':
+                        lst += tp.replace(type, {
+                            id: id,
+                            body: el.get('body')
+                        });
+                        break;
+                }
+            }, this);
+
+            return lst;
         },
     }, {
         ATTRS: {
             component: {value: COMPONENT},
-            templateBlockName: {value: 'widget'},
+            templateBlockName: {
+                value: 'widget,page,section,text'
+            },
             docid: NS.ATTRIBUTE.docid,
+            doc: NS.ATTRIBUTE.doc
         },
         parseURLParam: function(args){
             return {
